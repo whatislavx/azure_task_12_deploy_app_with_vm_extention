afa560e53ada4e571fc13f670948583f81efc96f
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-20 13:32:48 +0200

    Refactor: update start.sh and install-app.sh for improved setup; modify task.ps1 for Azure deployment and generated artifacts.json

--- install-app.sh
+++ install-app.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Script to silently install and start the todo web app on the virtual machine. 
-# Note that all commands bellow are without sudo - that's because extention mechanism 
-# runs scripts under root user. 
-
-# install system updates and isntall python3-pip package using apt. '-yq' flags are 
-# used to suppress any interactive prompts - we won't be able to confirm operation 
-# when running the script as VM extention.  
 apt-get update -yq
-apt-get install python3-pip -yq
+apt-get install python3-pip python3-venv git -yq
 
-# Create a directory for the app and download the files. 
-mkdir /app 
-# make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
-cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
+rm -rf /app
+mkdir /app
 
-# create a service for the app via systemctl and start the app
+cd /tmp
+rm -rf azure_task_12_deploy_app_with_vm_extention
+git clone https://github.com/whatislavx/azure_task_12_deploy_app_with_vm_extention.git
+
+find /tmp/azure_task_12_deploy_app_with_vm_extention/app -type f -exec sed -i 's/\r$//' {} +
+
+cp -r /tmp/azure_task_12_deploy_app_with_vm_extention/app/* /app/
+
 mv /app/todoapp.service /etc/systemd/system/
+
+chmod +x /app/start.sh
+
 systemctl daemon-reload
 systemctl start todoapp
 systemctl enable todoapp
+
+rm -rf /tmp/azure_task_12_deploy_app_with_vm_extention
--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "canadacentral"
 $resourceGroupName = "mate-azure-task-12"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -6,12 +6,12 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~\.ssh\id_rsa.pub" -Raw
 $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$dnsLabel = "matetask" + (Get-Random -Count 1)
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -26,7 +26,7 @@ New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroup
 
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
+New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
 
 New-AzVm `
 -ResourceGroupName $resourceGroupName `
@@ -39,4 +39,18 @@ New-AzVm `
 -SecurityGroupName $networkSecurityGroupName `
 -SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
 
-# ↓↓↓ Write your code here ↓↓↓
+$Params = @{
+    ResourceGroupName  = $resourceGroupName
+    VMName             = $vmName
+    Name               = 'customscriptextension'
+    Publisher          = 'Microsoft.Azure.Extensions'
+    ExtensionType      = 'CustomScript'
+    TypeHandlerVersion = '2.1'
+    Location           = $location
+    ProtectedSettings  = @{
+        fileUris = @('https://raw.githubusercontent.com/whatislavx/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh')
+        commandToExecute = './install-app.sh'
+    }
+}
+
+Set-AzVMExtension @Params
